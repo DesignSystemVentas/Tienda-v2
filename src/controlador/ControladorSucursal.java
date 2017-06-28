2b2d26513271ab8ca83099b2866e92c1d4959bb9
vizcarra <you@example.com>
2017-06-28 05:33:02 -0300

    agregue metodo llenarsucursal para llenar un combobox

--- src/controlador/ControladorSucursal.java
+++ src/controlador/ControladorSucursal.java
@@ -95,4 +95,14 @@ public class ControladorSucursal {
         }        
         return datos;
     }
+    //vizcarra
+    public ResultSet llenarSucursal() throws ErrorTienda {
+         Conexion cn = new Conexion();
+         try{
+        return (cn.getValores("SELECT * FROM sucursal"));
+        } catch (Exception ex){
+            throw new ErrorTienda("Error al traer sucursal al combobox" + ex.getMessage());
+        }  
+    }
+    //vizcarra
 }
