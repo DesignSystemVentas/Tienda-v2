2746381bfbf20f4acb166453f0740bf29ef3e4da
vizcarra <you@example.com>
2017-07-02 18:20:00 -0300

    modificando mi parte Vizcarra X3

--- src/controlador/ControladorTipoPrecio.java
+++ src/controlador/ControladorTipoPrecio.java
@@ -93,15 +93,5 @@ public class ControladorTipoPrecio {
          }
          return d;
      }
-     //vizcarra//
-    public ResultSet llenarUtilidad() throws ErrorTienda {
-         Conexion cn = new Conexion();
-         try{
-        return (cn.getValores("SELECT * FROM tipoprecio"));
-        } catch (Exception ex){
-            throw new ErrorTienda("Error al traer utilidad al combobox" + ex.getMessage());
-        }  
-    }
-    //vizcarra//
-     
+    
 }
